f764e06b77ca7064a91b664c9d7a5e44c45db6c9
ChibiZuki2 <m.morenoastete@gmail.com>
2026-07-27 06:21:14 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE = 'my-merakiyo-v2';
+const SHELL = ['/index.html', '/cuerpo.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(SHELL)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+  );
+  self.clients.claim();
+});
+
+/* Estrategia: red primero (para que los datos de Supabase siempre
+   intenten ser frescos); si no hay internet, cae al archivo cacheado
+   para que la app al menos abra (aunque los datos no se actualicen). */
+self.addEventListener('fetch', (e) => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        const resClone = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, resClone));
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
